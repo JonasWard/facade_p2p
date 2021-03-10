4431adc78b337a059aec2e894ec846294492dc7d
Jonas Ward <ward@ugd.ai>
2021-03-10 23:23:05 +0100

    with memory growth restriction

--- src/gpu_mat.py
+++ src/gpu_mat.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+# print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
-tf.debugging.set_log_device_placement(True)
+# tf.debugging.set_log_device_placement(True)
 
 # Create some tensors
 a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
--- src/understanding_oli.py
+++ src/understanding_oli.py
@@ -1,5 +1,18 @@
 f_path="C:\\Users\\oli\\Documents\\reps\\facade_p2p\\data\\labelmefacade-master\\labels\\sub_set"
 import p2p
+import tensorflow as tf
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+  # Restrict TensorFlow to only use the first GPU
+  try:
+    tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+    tf.config.experimental.set_memory_growth(gpus[0], False)
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+  except RuntimeError as e:
+    # Visible devices must be set before GPUs have been initialized
+    print(e)
 
 p2p_model=p2p.main(
     epochs=2,
