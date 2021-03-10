cec886dbf0fd33c664c443227fd25582aadb1857
Jonas Ward <ward@ugd.ai>
2021-03-10 21:25:38 +0100

    oli test

---
+++ src/understanding_oli.py
@@ -0,0 +1,9 @@
+f_path="C:\\Users\\oli\\Documents\\reps\\facade_p2p\\data\\labelmefacade-master\\labels\\sub_set"
+
+p2p_model=p2p.main(
+    epochs=2,
+    enable_function=True,
+    path=f_path,
+    buffer_size=200,
+    batch_size=1
+)
