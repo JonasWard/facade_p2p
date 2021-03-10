15d8194fa53d5f446ba324f27dc482e033e3ecfa
Jonas Ward <ward@ugd.ai>
2021-03-10 23:25:20 +0100

    set memery growth true

--- src/understanding_oli.py
+++ src/understanding_oli.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 
 gpus = tf.config.list_physical_devices('GPU')
 for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, False)
+    tf.config.experimental.set_memory_growth(gpu, True)
 # if gpus:
 #   # Restrict TensorFlow to only use the first GPU
 #   try:
